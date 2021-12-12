71a3918e1a3619d644fe6620999eaa2f21c6827a
levi erentry <95186801+LeviErentry@users.noreply.github.com>
2021-12-12 19:27:36 +0200

    revert changes

--- Infrastructure/Host.cs
+++ Infrastructure/Host.cs
@@ -15,20 +15,15 @@ namespace Infrastructure
 
         public string Run(string input, string user)
         {
-
-            // TODO: If plugin number not in range throw exception
-            // TOOD: Show help only when help requested
             if (!int.TryParse(input, out int pluginNumber) || pluginNumber > PluginsManager.plugins.Count || pluginNumber <= 0)
             {
                 return new PluginsMenu().PlaginsHelp();
             }
 
             var pluginId = PluginsManager.plugins[pluginNumber - 1];
-            // TODO: No new
             var plugin = new PluginsManager().CreatePlugin(pluginId);
             var session = _dal.LoadData(user, pluginId);
 
-            // TODO: input should be passed to plugin?
             var output = plugin.Execute("", session, null);
             _dal.SaveData(user, pluginId, output.Session);
 
