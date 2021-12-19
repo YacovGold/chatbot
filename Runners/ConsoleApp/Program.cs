998ed0e800b3faf8a872beb4374b1a6766ce3a7e
levi erentry <95186801+LeviErentry@users.noreply.github.com>
2021-12-19 12:50:01 +0200

    Printing correction messages

--- Runners/ConsoleApp/Program.cs
+++ Runners/ConsoleApp/Program.cs
@@ -16,9 +16,11 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             var host = new Host(new MemoryDal(), new PluginsMenu(), new PluginsManager());
+            Console.WriteLine(host.Run("HelP", ""));
             Console.WriteLine(host.Run("3", ""));
             Console.WriteLine(host.Run("3", ""));
             Console.WriteLine(host.Run("7", ""));
+            Console.WriteLine(host.Run("ht", ""));
 
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
