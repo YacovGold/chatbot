ef584a600376e9cae3c9aa192ea85dc3332bfae9
YacovGold <8984042+YacovGold@users.noreply.github.com>
2022-01-02 19:30:08 +0200

    Change host to PluginExecutor Embed CallbacksProxy into PluginExecutor Make naming clearer

--- Runners/ConsoleApp/Program.cs
+++ Runners/ConsoleApp/Program.cs
@@ -15,19 +15,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var host = new Host(new MemoryDal(), new PluginsMenu(), new PluginsManager());
-            Console.WriteLine(host.Run("HelP", ""));
+            var pluginExecutor = new PluginExecutor(new MemoryDal(), new PluginsMenu(), new PluginsManager());
+            Console.WriteLine(pluginExecutor.Run("HelP", ""));
 
-            Console.WriteLine(host.Run("4", ""));
-            Console.WriteLine(host.Run("test", ""));
-            Console.WriteLine(host.Run("again", ""));
-            Console.WriteLine(host.Run("and again...", ""));
-            Console.WriteLine(host.Run("exit", ""));
+            Console.WriteLine(pluginExecutor.Run("4", ""));
+            Console.WriteLine(pluginExecutor.Run("test", ""));
+            Console.WriteLine(pluginExecutor.Run("again", ""));
+            Console.WriteLine(pluginExecutor.Run("and again...", ""));
+            Console.WriteLine(pluginExecutor.Run("exit", ""));
 
-            Console.WriteLine(host.Run("3", ""));
-            Console.WriteLine(host.Run("3", ""));
-            Console.WriteLine(host.Run("7", ""));
-            Console.WriteLine(host.Run("ht", ""));
+            Console.WriteLine(pluginExecutor.Run("3", ""));
+            Console.WriteLine(pluginExecutor.Run("3", ""));
+            Console.WriteLine(pluginExecutor.Run("7", ""));
+            Console.WriteLine(pluginExecutor.Run("ht", ""));
 
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
