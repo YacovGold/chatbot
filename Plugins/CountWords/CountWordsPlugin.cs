dab9b99cecc2a86d437845d767e3946879ab58bc
meir@ccc.co.il <meir@ccc.co.il>
2022-01-16 12:46:53 +0200

    add count down plugin

---
+++ Plugins/CountWords/CountWordsPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using BasePlugin;
+using BasePlugin.Interfaces;
+using BasePlugin.Records;
+
+
+namespace CountWords
+{
+    record Session(int NumberOfWords);
+    public class CountWordsPlugin : IPlugin
+    {
+        public string Id => throw new NotImplementedException();
+
+
+        public bool CanExecute(string input, string session)
+        {
+            return true;
+        }
+
+        public PluginOutput Execute(PluginInput input)
+        {
+            int numberOfWords = input.Message.Split(' ').ToList().Where(s => !string.IsNullOrEmpty(s)).Count();
+            var ses = new Session(numberOfWords);
+            return new PluginOutput("The number of words is " + numberOfWords, JsonSerializer.Serialize(ses));
+        }
+    }
+}
