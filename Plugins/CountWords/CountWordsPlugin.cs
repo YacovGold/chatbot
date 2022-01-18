e3d975194b546b6581e16a112818e07473302203
ymeir21@gmail.com <ymeir21@gmail.com>
2022-01-18 10:47:24 +0200

    Changes after code review

--- Plugins/CountWords/CountWordsPlugin.cs
+++ Plugins/CountWords/CountWordsPlugin.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Text.Json;
-using BasePlugin;
 using BasePlugin.Interfaces;
 using BasePlugin.Records;
 
@@ -9,22 +8,19 @@ using BasePlugin.Records;
 
 namespace CountWords
 {
-    record Session(int NumberOfWords);
+   
     public class CountWordsPlugin : IPlugin
     {
         public string Id => throw new NotImplementedException();
 
 
-        public bool CanExecute(string input, string session)
-        {
-            return true;
-        }
+      
 
         public PluginOutput Execute(PluginInput input)
         {
-            int numberOfWords = input.Message.Split(' ').ToList().Where(s => !string.IsNullOrEmpty(s)).Count();
-            var ses = new Session(numberOfWords);
-            return new PluginOutput("The number of words is " + numberOfWords, JsonSerializer.Serialize(ses));
+            var numberOfWords = input.Message.Split(' ').Where(s => !string.IsNullOrEmpty(s)).Count();
+            
+            return new PluginOutput("The number of words is " + numberOfWords,numberOfWords.ToString());
         }
     }
 }
