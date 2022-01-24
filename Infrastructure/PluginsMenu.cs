0f3b248f6be695894769be1eed7d148254b81a7b
ymeir21@gmail.com <ymeir21@gmail.com>
2022-01-24 20:03:30 +0200

    clear code

--- Infrastructure/PluginsMenu.cs
+++ Infrastructure/PluginsMenu.cs
@@ -9,7 +9,7 @@ namespace Infrastructure
 {
     public class PluginsMenu
     {
-        public string PlaginsHelp()
+        public string PluginsHelp()
         {
             var lines = PluginsManager.plugins.Select((p, i) => $"{i + 1}. {p}").ToList();
             lines.Add("Sample: 4 the letters will be in upper case");
