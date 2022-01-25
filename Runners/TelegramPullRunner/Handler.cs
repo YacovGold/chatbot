a3a1a3274c1a30176e6bf724164f3af8d494815a
shabi mak <shabimak@gmail.com>
2022-01-25 21:31:48 +0200

    Add a Telegram pull runner thac exectue the system.

---
+++ Runners/TelegramPullRunner/Handler.cs
@@ -0,0 +1,49 @@
+﻿using Dals;
+using Infrastructure;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+namespace Telegram.Bot.Examples.Polling
+{
+    public class Handlers
+    {
+        private static PluginExecutor pluginExecutor = new PluginExecutor(new MemoryDal(), new PluginsMenu(), new PluginsManager());
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = BotOnMessageReceived(botClient, update.Message!);
+            try
+            {
+                await handler;
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, cancellationToken);
+            }
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            Console.WriteLine($"Receive message type: {message.Type}");
+            if (message.Type != MessageType.Text)
+                return;
+            var res = pluginExecutor.Run(message.Text, message.Chat.Id.ToString());
+            var action = botClient.SendTextMessageAsync(chatId: message.Chat.Id, res);
+            Message sentMessage = await action;
+        }
+    }
+}
