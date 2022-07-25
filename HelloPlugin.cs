915b73a5d6d91ffacfb0a88b5f7a24d47b55b5c0
shaiRubin770 <108010953+shaiRubin770@users.noreply.github.com>
2022-07-25 13:33:48 +0300

    Add files via upload

---
+++ HelloPlugin.cs
@@ -0,0 +1,19 @@
+﻿using BasePlugin.Interfaces;
+using BasePlugin.Records;
+using System;
+
+namespace Hello
+{
+    public class HelloPlugin : IPlugin
+    {
+        public static string _Id = "hello";
+        public string Id => _Id;
+        public PluginOutput Execute(PluginInput input)
+        {
+            var result = new PluginOutput("Hello");
+            return result;
+        }
+    }
+}
+
+   
