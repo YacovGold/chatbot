54ab46f02cf454314ab9c2556e6f6f5579685638
ShaiRubin <108010953+shaiRubin770@users.noreply.github.com>
2022-08-09 21:30:32 +0300

    new plugin

--- Plugins/IsPrimeNumber/IsPrimeNumberPlugin.cs
+++ Plugins/IsPrimeNumber/IsPrimeNumberPlugin.cs
@@ -10,7 +10,35 @@ namespace IsPrimeNumber
     
     public void Execute(PluginInput input)
         {
-            throw new NotImplementedException();
+            
+            bool divider;
+
+            if(input.Message==string.Empty)
+            {
+                input.Callbacks.StartSession();
+                input.Callbacks.SendMessage("Please type a number: ");
+            }
+            else if (input.Message.ToLower() == "exit")
+            {
+                input.Callbacks.EndSession();
+                input.Callbacks.SendMessage("we are sorry that you leave");
+            }
+            else if(int.TryParse(input.Message,out int num1))
+            {
+                for (int i = 2; i < Math.Sqrt(num1); i++)
+                {
+                    divider = num1 % i == 0;
+
+                    if (divider)
+                        input.Callbacks.SendMessage(num1 + " is not a prime number, because is divide in " + i);
+
+                }
+
+            }
+            else
+            {
+                input.Callbacks.SendMessage("try again, enter a number");
+            }
         }
     }
 }
