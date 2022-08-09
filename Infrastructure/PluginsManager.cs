fc3b19c096a771b309030d0fbe21ebb23646036c
ShaiRubin <108010953+shaiRubin770@users.noreply.github.com>
2022-08-09 20:39:08 +0300

    is prime number plugin

--- Infrastructure/PluginsManager.cs
+++ Infrastructure/PluginsManager.cs
@@ -15,6 +15,7 @@ using List;
 using Timers;
 using DataTimeNow;
 using NumerologyCalculator;
+using IsPrimeNumber;
 
 namespace Infrastructure
 {
@@ -66,6 +67,10 @@ namespace Infrastructure
             {
                 return new NumerologyCalculatorPlugin();
             }
+            else if (id == IsPrimeNumberPlugin._Id)
+            {
+                return new IsPrimeNumberPlugin();
+            }
             else
             {
                 throw new NotImplementedException();
@@ -84,7 +89,8 @@ namespace Infrastructure
             TimersPlugin._Id,
             AlarmClockPlugin._Id,
             Date._Id,
-            NumerologyCalculatorPlugin._Id
+            NumerologyCalculatorPlugin._Id,
+            IsPrimeNumberPlugin._Id
         };
     }
 }
---
+++ Plugins/IsPrimeNumber/IsPrimeNumberPlugin.cs
@@ -0,0 +1,16 @@
+﻿using BasePlugin.Interfaces;
+using BasePlugin.Records;
+
+namespace IsPrimeNumber
+{
+    public class IsPrimeNumberPlugin : IPlugin
+    {
+        public static string _Id => "Is Prime Number";
+        public string Id => _Id;
+    
+    public void Execute(PluginInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
