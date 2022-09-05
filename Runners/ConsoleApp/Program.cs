1fad75d11c9e530a8ce48344f24bccfaff7c6444
YacovGold <8984042+YacovGold@users.noreply.github.com>
2022-09-05 20:55:05 +0300

    Revert program.cs

--- Runners/ConsoleApp/Program.cs
+++ Runners/ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using BasePlugin;
+using BasePlugin;
 using BasePlugin.Interfaces;
 using BasePlugin.Records;
 using CountDown;
@@ -9,26 +9,35 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace ConsoleApp
 {
+
     class Program : IMessageSender
     {
+
         static void Main()
         {
             Program program = new Program();
             program.Main(program);
         }
+
         void Main(IMessageSender messageSender)
         {
-            var pluginExecutor = new PluginExecutor(messageSender, new DbDal(), new PluginsMenu(), new PluginsManager());
-            Console.WriteLine("You can type help to get all the pulgins: ");
+            var pluginExecutor = new PluginExecutor(messageSender, new MemoryDal(), new PluginsMenu(), new PluginsManager());
+            Console.WriteLine("Hi! I am Your amazing bot and I will be happy to be of service to you on several topics.");
+            Console.WriteLine("Please type the number of the plugin you would like to help, or type help to get the list of options.");
             while (true)
             {
+                var PluginId = pluginExecutor.GetCurrentUserPluginId(" ");
+                PluginId = (PluginId != null && PluginId != "") ? "\\" + PluginId : "";
+                Console.Write("main{0}> ", PluginId);
                 var msg = Console.ReadLine();
                 pluginExecutor.Run(msg, " ");
             }
         }
+
         public void SendMessage(string userId, string data)
         {
             Console.WriteLine(data);
